9e7347ced8b30251a9267db3ba86864a333c16e8
Anne Belka and Steve Burton <anne+steve@>
2017-03-01 11:43:19 -0800

    add GetBreweries method

--- src/MapExplore/Controllers/HomeController.cs
+++ src/MapExplore/Controllers/HomeController.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MapExplore.Models;
 
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MapExplore.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult GetBreweries()
+        {
+            var allBreweries = Brewery.GetBreweries();
+            return View(allBreweries);
         }
     }
 }
---
+++ src/MapExplore/Models/Brewery.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+namespace MapExplore.Models
+{
+    public class Brewery
+    {
+        public string Lat { get; set; }
+        public string Long { get; set; }
+        public string Name { get; set; }
+
+        public void GetBreweries()
+        {
+            var client = new RestClient("http://api.brewerydb.com/v2");
+            var request = new RestRequest("/location/d25euF/?key=65c0242a26aa3edd867b8ea8edaf7236", Method.GET);
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var brewery = JsonConvert.DeserializeObject<JObject>(jsonResponse["data[name]"].ToString());
+            Debug.WriteLine(brewery);
+            
+
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
